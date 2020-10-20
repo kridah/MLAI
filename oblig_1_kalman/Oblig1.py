1b7f4f65d1672fc88ae12e2024063a23571312a3
kridah <kridah@gmail.com>
2020-10-20 09:56:33 +0200

    Oblig 1 - implementert kalman-filter. Over tid ligger kalman-score på +-0.80

--- oblig_1_kalman/Oblig1.py
+++ oblig_1_kalman/Oblig1.py
@@ -13,7 +13,6 @@ from numpy.linalg import norm
 
 fps = 0.0
 
-
 class Projectile():
 
     def __init__(self, kalman=None):
@@ -26,8 +25,8 @@ class Projectile():
 
     def move(self, goal):
 
-        #if self.kalm:
-       #     goal = self.kalm.calc_next(goal)
+        #  if self.kalm:
+        #      goal = self.kalm.calc_next(goal)
 
         deltax = np.array(float(goal) - self.px)
         # print(delta2)
@@ -35,8 +34,8 @@ class Projectile():
         np.divide(deltax, mag_delta, deltax)
 
         self.dx += deltax
-        #if self.dx:
-        #    self.dx /= norm(self.dx) * 50
+        #  if self.dx:
+        #     self.dx /= norm(self.dx) * 50
 
         self.px += self.dx / 50.0
         self.py += -0.5
@@ -70,25 +69,71 @@ class Target():
         noise = np.random.normal(0, 1, 1)[0]
         return pos + center + noise * 300.0
 
-
 #
 # Her er Kalmanfilteret du skal utvikle
 #
 class Kalman():
+    '''
+    # https://www.kalmanfilter.net/alphabeta.html
+    # https://arxiv.org/pdf/1204.0375.pdf
+    # Algoritmen er iterativ. Hvert trinn inkluderer to faser: prediksjon og måleoppdatering.
+    # Init: System state initial guess
+    # 1: Input: Målt verdi
+    # 2: Kalkuler Kalman gain (A). Estimer current state med state update equation. Output: System State Estimate ^Xn,n
+    # 3: Kalkuler forventet state for neste iterasjon. Unit delay (n -> n+1). ^Xn,n-1
+    # Gjenta 2 og 3
+    '''
 
-    def __init__(self, background, width):
-        self.background = background
-        self.rect = pg.Rect(self.background.get_width() // 2 - width // 2,
-                            50, width, 32)
-    '''Bevegelsen til det blå Kalman-missilet er det som dere skal utvikle. 
-    Denne utvikles som en egen klasse, 
-    Kalman som minimum må inneholde metoden calc_next(self, zi)
-     som returnerer den neste predikerte verdien basert på de støyfulle målingene.'''
+    # Enkelte verdier er basert på gjetting, prøving og feiling
+    def __init__(self):
+        self.alpha = 2 / 3
+        self.beta = 0.1
+        self.n = iters  # antall iterasjoner
+        self.dt = fps   # dt antall målinger i sekundet gitt i fps
+        self.current_iteration = 9   # xn_n
+        self.prediction = 0    # Xn,n + 1
+        self.previous_iteration = 0   # Xn,n - 1
+        self.xdhn_n = 1
+        self.xdhn_n_plus_1 = 0
+        self.xdhn_n_minus_1 = 0
+        self.z_n = 0.0
+
+    def state_extrapolation_equation(self):
+        self.prediction = self.current_iteration + (self.dt * self.xdhn_n)
+        self.xdhn_n_plus_1 = self.xdhn_n
+
+    def state_update_equation(self):
+        self.current_iteration = self.previous_iteration + self.alpha * (self.z_n - self.previous_iteration)
+
+        ''' Hver for seg gir de to neste regnestykkene stigende eller synkende kalmanscore.
+            Ved å bruke begge to får jeg mer konsistent resultat / kalmanscore.'''
+        self.xdhn_n = self.xdhn_n_minus_1 + self.beta * (
+                (self.z_n - self.previous_iteration) // self.dt)  # gir økende kalmanscore 0.0 -> 0.8
+        self.xdhn_n = self.xdhn_n_minus_1 + self.beta * (
+                (self.z_n - self.previous_iteration) / self.dt)  # gir minkende kalmanscore 0.4 -> 0.9
+
+    def measurement(self, value):
+        self.z_n = value
+
+    def iterate(self):
+        self.n += iters  # iterasjoner
+        # predikeringen fra forrige iterasjon settes til det forrige estimatet til nåværende iterasjon
+        self.previous_iteration = self.current_iteration
+        self.xdhn_n_minus_1 = self.xdhn_n
+        self.dt = fps
+
     def calc_next(self, zi):
-        print("Kalman ", zi)
-        return zi
+        # ved første iterering initialiserer man verdiene med kvalifisert gjetning
+        if self.n == 0:
+            self.state_extrapolation_equation()
+            self.previous_iteration = self.prediction
+            self.xdhn_n_minus_1 = self.xdhn_n_plus_1
 
-
+        self.measurement(zi)
+        self.state_update_equation()
+        self.state_extrapolation_equation()
+        self.iterate()
+        return self.current_iteration    # returnerer predikert posisjon til k_miss
 
 
 pg.init()
@@ -105,23 +150,18 @@ reg_score = 0
 iters = 0
 
 while running:
-    count = 0.0
-    xpos_sum = 0.0
     target = Target(background, 32)
     missile = Projectile(background)
-    kalman = Kalman(background, 32)
-    k_miss = Projectile(background) #kommenter inn denne linjen naar Kalman er implementert
-    #k_miss = Projectile(background, 3)
-    last_x_pos = target.noisy_x_pos
+    kalman = Kalman()
+    k_miss = Projectile(background)  # kommenter inn denne linjen naar Kalman er implementert
     noisy_draw = np.zeros((w, 20))
 
     trial = True
     iters += 1
 
     while trial:
-
         # Setter en maksimal framerate på 300. Hvis dere vil øke denne er dette en mulig endring
-        clock.tick(100)
+        clock.tick(300)
         fps = clock.get_fps()
 
         for e in pg.event.get():
@@ -131,20 +171,14 @@ while running:
         background.fill(0x448844)
         surf[:, 0:20, 0] = noisy_draw
 
-        # Summer posisjonen og tell antall målinger
-
-        last_x_pos = target.noisy_x_pos()
-        print("last_x_pos", last_x_pos)
-        xpos_sum += last_x_pos
-        count += 1.0
+        last_x_pos = target.noisy_x_pos()  # dette er støyen vi skal filtrere
 
         target.move()
         missile.move(last_x_pos)
-        kal_last_x_pos = kalman.calc_next(xpos_sum / count)
-        k_miss.move(kal_last_x_pos) #kommenter inn denne linjen naar Kalman er implementert
+        k_miss.move(kalman.calc_next(last_x_pos))  # kommenter inn denne linjen naar Kalman er implementert
 
         pg.draw.rect(background, (255, 200, 0), missile.rect)
-        pg.draw.rect(background, (0, 200, 255), k_miss.rect) #kommenter inn denne linjen naar Kalman er implementert
+        pg.draw.rect(background, (0, 200, 255), k_miss.rect)  # kommenter inn denne linjen naar Kalman er implementert
         pg.draw.rect(background, (255, 200, 255), target.rect)
 
         noisy_draw[int(last_x_pos):int(last_x_pos) + 20, :] = 255
@@ -152,22 +186,23 @@ while running:
         np.clip(noisy_draw, 0, 255, noisy_draw)
 
         coll = missile.rect.colliderect(target.rect)
-        k_coll = k_miss.rect.colliderect(target.rect) #kommenter inn denne linjen naar Kalman er implementert#
+        k_coll = k_miss.rect.colliderect(target.rect)  # kommenter inn denne linjen naar Kalman er implementert#
 
         if coll:
             reg_score += 1
 
-        # if k_coll:    #kommenter inn denne linjen naar Kalman er implementert
-        #     kalman_score += 1
+        if k_coll:  # kommenter inn denne linjen naar Kalman er implementert
+            kalman_score += 1
 
         oob = missile.rect.y < 20
 
-        if oob or coll:  # or k_coll #endre denne sjekken slik at k_coll ogsaa er med naar kalman er implementert
+        # runden er over
+        if oob or coll or k_coll:  # or k_coll #endre denne sjekken slik at k_coll ogsaa er med naar kalman er implementert
             trial = False
 
         pg.display.flip()
 
-    # print('kalman score: ', round(kalman_score/iters,2)) #kommenter inn denne linjen naar Kalman er implementert
+    print('kalman score: ', round(kalman_score / iters, 2))  # kommenter inn denne linjen naar Kalman er implementert
     print('regular score: ', round(reg_score / iters, 2))
 
 pg.quit()
